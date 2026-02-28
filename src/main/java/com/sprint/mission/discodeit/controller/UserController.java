25f11922d65d1329246208b35f8fffa530ebd408
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-03-01 03:15:51 +0900

    feat(UserController): update for api spec

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -1,12 +1,23 @@
 package com.sprint.mission.discodeit.controller;
 
-import com.sprint.mission.discodeit.dto.UserServiceDTO.UserCreateRequest;
-import com.sprint.mission.discodeit.dto.UserServiceDTO.UserResponse;
-import com.sprint.mission.discodeit.dto.UserServiceDTO.UserUpdateRequest;
+import com.sprint.mission.discodeit.dto.userstatus.UserStatusServiceDTO.UserStatusResponse;
+import com.sprint.mission.discodeit.dto.userstatus.command.UserStatusUpdateCommand;
+import com.sprint.mission.discodeit.dto.userstatus.request.UserStatusUpdateRequest;
+import com.sprint.mission.discodeit.dto.user.UserServiceDTO.UserResponse;
+import com.sprint.mission.discodeit.dto.user.command.UserCreateCommand;
+import com.sprint.mission.discodeit.dto.user.command.UserUpdateCommand;
+import com.sprint.mission.discodeit.dto.user.request.UserCreateRequest;
+import com.sprint.mission.discodeit.dto.user.request.UserUpdateRequest;
 import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.UserStatusService;
+import jakarta.annotation.Nullable;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
@@ -16,35 +27,45 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final UserStatusService userStatusService;
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/{id}")
     public ResponseEntity<UserResponse> find(@PathVariable UUID id) {
-        return ResponseEntity.ok(userService.find(id));
+        return ResponseEntity.status(HttpStatus.OK).body(userService.find(id));
     }
 
-    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<List<UserResponse>> findAll() {
-        return ResponseEntity.ok(userService.findAll());
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
     }
 
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<UserResponse> create(@RequestBody UserCreateRequest request) {
-        return ResponseEntity.status(201).body(userService.create(request));
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<UserResponse> create(
+            @RequestPart @Valid UserCreateRequest userCreateRequest,
+            @RequestPart @Nullable MultipartFile profile) {
+        UserCreateCommand command = new UserCreateCommand(userCreateRequest, profile);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(command));
     }
 
-    @RequestMapping(method = RequestMethod.PUT)
-    public ResponseEntity<UserResponse> update(@RequestBody UserUpdateRequest request) {
-        return ResponseEntity.ok(userService.update(request));
+    @PatchMapping(value = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<UserResponse> update(
+            @PathVariable UUID id,
+            @RequestPart @Valid UserUpdateRequest userUpdateRequest,
+            @RequestPart @Nullable MultipartFile profile) {
+        UserUpdateCommand command = new UserUpdateCommand(id, userUpdateRequest, profile);
+        return ResponseEntity.status(HttpStatus.OK).body(userService.update(command));
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<?> delete(@PathVariable UUID id) {
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
         userService.delete(id);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @RequestMapping(value = "/{id}/active", method = RequestMethod.PATCH)
-    public ResponseEntity<UserResponse> updateActiveStatus(@PathVariable UUID id) {
-        return ResponseEntity.ok(userService.updateActiveAt(id));
+    @PatchMapping(value = "/{id}/userStatus")
+    public ResponseEntity<UserStatusResponse> updateActiveStatus(@PathVariable UUID id,
+                                                                 @RequestBody @Valid UserStatusUpdateRequest request) {
+        UserStatusUpdateCommand command = new UserStatusUpdateCommand(id, request.datetime());
+        return ResponseEntity.status(HttpStatus.OK).body(userStatusService.update(command));
     }
 }
