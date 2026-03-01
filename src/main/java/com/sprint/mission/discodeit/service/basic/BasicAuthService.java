4c24d28b0d7804185eefa72dad621923ac805c8a
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-03-01 21:04:47 +0900

    feat(controller): update AuthController

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -1,6 +1,9 @@
 package com.sprint.mission.discodeit.service.basic;
 
+import com.sprint.mission.discodeit.common.exception.code.ErrorCode;
+import com.sprint.mission.discodeit.common.exception.custom.APIException;
 import com.sprint.mission.discodeit.dto.AuthServiceDTO.LoginRequest;
+import com.sprint.mission.discodeit.dto.user.UserServiceDTO.UserResponse;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.AuthService;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +15,12 @@ public class BasicAuthService implements AuthService {
     private final UserRepository userRepository;
 
     @Override
-    public boolean login(LoginRequest request) {
+    public UserResponse login(LoginRequest request) {
         return userRepository.filter(user -> user.matchUsername(request.username()))
-                .anyMatch(user -> user.matchPassword(request.password()));
+                .filter(user -> user.matchPassword(request.password()))
+                .findFirst()
+                .orElseThrow(() -> new APIException(ErrorCode.USERNAME_OR_PASSWORD_INCORRECT))
+                .toResponse(true);
+
     }
 }
