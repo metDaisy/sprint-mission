aa42fb85f80450bf35fa18d1f95cd01341dd3ec9
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-03-06 15:29:44 +0900

    feat(service): update UserService
    - UserService: command -> dto
    - BasicUserService: remove IdGenerator
    - BasicUserService: suspending 'find' because not including api specs
    - BasicUserService: convert entity <-> dto using mapper

--- src/main/java/com/sprint/mission/discodeit/service/DomainService.java
+++ src/main/java/com/sprint/mission/discodeit/service/DomainService.java
@@ -3,11 +3,11 @@ package com.sprint.mission.discodeit.service;
 import java.util.UUID;
 
 public interface DomainService<F, C, U> {
-    F create(C model);
+    F create(C dto);
 
     F find(UUID id);
 
-    F update(U model);
+    F update(U dto);
 
     void delete(UUID id);
 }
--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -1,15 +1,14 @@
 package com.sprint.mission.discodeit.service;
 
+import com.sprint.mission.discodeit.dto.user.UserServiceDTO.UserCreateDto;
 import com.sprint.mission.discodeit.dto.user.UserServiceDTO.UserResponse;
-import com.sprint.mission.discodeit.dto.user.command.UserCreateCommand;
-import com.sprint.mission.discodeit.dto.user.command.UserUpdateCommand;
+import com.sprint.mission.discodeit.dto.user.UserServiceDTO.UserUpdateDto;
 import com.sprint.mission.discodeit.dto.user.request.UserFindRequest;
 
 import java.util.List;
 
-public interface UserService extends DomainService<UserResponse, UserCreateCommand, UserUpdateCommand> {
+public interface UserService extends DomainService<UserResponse, UserCreateDto, UserUpdateDto> {
     UserResponse find(UserFindRequest request);
 
     List<UserResponse> findAll();
-
 }
