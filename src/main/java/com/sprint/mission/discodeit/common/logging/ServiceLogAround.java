bb486293ceca9e40c251c4a7ca74126ce405bfd1
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-04-10 22:23:47 +0900

    feat: 서비스 계층 AOP 로깅 구현
    --- 본문 ---
    `ServiceLogAround` 어노테이션이 적용된 서비스 메서드의 실행 시간, 인자, 결과 및 에러를 로깅하는 AOP 기능을 추가합니다.
    이를 위해 `spring-boot-starter-aop` 의존성을 추가했습니다.

---
+++ src/main/java/com/sprint/mission/discodeit/common/logging/ServiceLogAround.java
@@ -0,0 +1,12 @@
+package com.sprint.mission.discodeit.common.logging;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ServiceLogAround {
+
+}
