9271556714f9ea5233e4eceb0bc619bf99769819
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-06-20 17:24:28 +0900

    refactor: BaseEntity equals/hashCode 리팩토링 및 중복 제거
    --- 본문 ---
    - BaseEntity의 equals 및 hashCode 메서드를 Hibernate 프록시를 고려하여 재작성했습니다.
    - JPA 엔티티 비교 시 발생할 수 있는 문제를 방지하고, 상속 관계에서 올바른 동등성 비교를 보장합니다.
    - BinaryContent와 RefreshToken 엔티티에서 중복되던 equals 및 hashCode 구현을 제거하여 코드의 일관성과 유지보수성을 높였습니다.

--- src/main/java/com/sprint/mission/discodeit/common/jpa/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/common/jpa/BaseEntity.java
@@ -9,6 +9,7 @@ import java.time.Instant;
 import java.util.Objects;
 import java.util.UUID;
 import lombok.Getter;
+import org.hibernate.proxy.HibernateProxy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.domain.Persistable;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -31,17 +32,30 @@ public abstract class BaseEntity implements Persistable<UUID> {
   }
 
   @Override
-  public boolean equals(Object o) {
-    if (o == null || getClass() != o.getClass()) {
+  public final boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null) {
+      return false;
+    }
+    Class<?> oEffectiveClass = o instanceof HibernateProxy
+        ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass()
+        : o.getClass();
+    Class<?> thisEffectiveClass = this instanceof HibernateProxy
+        ? ((HibernateProxy) this).getHibernateLazyInitializer()
+        .getPersistentClass() : this.getClass();
+    if (thisEffectiveClass != oEffectiveClass) {
       return false;
     }
     BaseEntity that = (BaseEntity) o;
-    return Objects.equals(id, that.id);
+    return getId() != null && Objects.equals(getId(), that.getId());
   }
 
   @Override
-  public int hashCode() {
-    return Objects.hashCode(id);
+  public final int hashCode() {
+    return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer()
+        .getPersistentClass().hashCode() : getClass().hashCode();
   }
 
   @Override
